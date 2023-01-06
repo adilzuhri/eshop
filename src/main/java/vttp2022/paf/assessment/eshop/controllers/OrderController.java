1580db051e5d6a4aaa70dbeda69ca143d6a54413
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2023-01-06 15:32:50 +0800

    Task 5 attempted

--- src/main/java/vttp2022/paf/assessment/eshop/controllers/OrderController.java
+++ src/main/java/vttp2022/paf/assessment/eshop/controllers/OrderController.java
@@ -1,7 +1,90 @@
 package vttp2022.paf.assessment.eshop.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import vttp2022.paf.assessment.eshop.models.Order;
+import vttp2022.paf.assessment.eshop.services.OrderService;
+
+@RestController
+@RequestMapping(path = "/order/total", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
-	//TODO: Task 3
+    @Autowired
+    private OrderService odSvc;
 
+    @GetMapping(path = "{orderId}")
+    public ResponseEntity<String> getOrderId(@PathVariable Integer orderId) {
+        Order ord = odSvc.getOrderId(orderId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ord.toJSON().toString());
+    }
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> putRSVP(@RequestBody String json) {
+        Order order = null;
+        boolean orderResult = false;
+        JsonObject resp;
+        try {
+            order = ORDER.create(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp = Json.createObjectBuilder()
+                    .add("error", e.getMessage())
+                    .build();
+            return ResponseEntity.badRequest().body(resp.toString());
+        }
+
+        orderResult = (boolean) odSvc.updateOrder(order);
+        resp = Json.createObjectBuilder()
+                .add("updated", orderResult)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(resp.toString());
+    }
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postOrder(@RequestBody String json) {
+        Order order = null;
+        Order orderResult = null;
+        JsonObject resp;
+        try {
+            order = ORDER.create(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp = Json.createObjectBuilder()
+                    .add("error", e.getMessage())
+                    .build();
+            return ResponseEntity.badRequest().body(resp.toString());
+        }
+
+        orderResult = odSvc.insertOrder(order);
+        resp = Json.createObjectBuilder()
+                .add("dispatchId", orderResult.getOrderId())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(resp.toString());
+    }
 }
--- src/main/java/vttp2022/paf/assessment/eshop/repositories/Queries.java
+++ src/main/java/vttp2022/paf/assessment/eshop/repositories/Queries.java
@@ -1,9 +1,27 @@
 package vttp2022.paf.assessment.eshop.repositories;
 
 public class Queries {
-   
-        public static final String SQL_SELECT_CUSTOMERS_BY_NAME = "select name, address, email from customers where name = ?";
-    
-        // public static final String SQL_SELECT_ALL_CUSTOMERS_ORDER = "select c.id as customer_id, company, c.last_name, c.first_name, c.email_address, c.job_title, c.business_phone, c.home_phone, c.mobile_phone, c.address, c.state_province, o.id as order_id, DATE_FORMAT(o.order_date, \"%d/%m/%Y\") as order_date, DATE_FORMAT(o.shipped_date, \"%d/%m/%Y\") as shipped_date, o.ship_name, o.shipping_fee from customers c, orders o where c.id = o.customer_id and o.customer_id = ?";
- 
-}
+
+  public static final String SQL_SELECT_CUSTOMERS_BY_NAME = "select name, address, email from customers where name = ?";
+
+  public static final String SQL_ORDERS_BY_CUSTOMER = """
+        SELECT
+                o.id as order_id,
+                DATE_FORMAT(o.order_date, \"%d/%m/%Y\") as order_date,
+                o.customer_id as customer_id,
+                sum(od.quantity * od.unit_price) as total_price,
+                sum(od.quantity * od.unit_price * od.discount) as discount,
+                sum(od.quantity * od.unit_price) - sum(od.quantity * od.unit_price * od.discount) as discounted_price,
+                sum(od.quantity * p.standard_cost) as cost_price
+        FROM
+                orders o
+                LEFT JOIN order_details od
+                ON o.id = od.order_id
+                LEFT JOIN products p
+                ON od.product_id = p.id
+                WHERE o.id = ?
+                """;
+            
+            }
+            
+
